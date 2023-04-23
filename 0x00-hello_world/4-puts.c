e0753f7f133ae97fa008284daf39daa60527ef4a
TheEikoSystem <gloryedwinkpo@gmail.com>
2023-04-23 05:13:43 -0700

    far gone

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * A program that is truely inspired by betty
- * Return: 0
- * /
+ * main - A program that prints a line with puts function
+ * Return: 0 (Success)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
