59d77f03096ac123f0fba4e5dd049e17a1c86b5b
TheEikoSystem <gloryedwinkpo@gmail.com>
2023-04-26 14:59:22 -0700

    eiko

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - adds two integers
+ * @j: number to be added
+ * @k: number to also be added
+ * Return: 0 (success)
+ */
+int add(int j, int k)
+{
+	return (j + k);
+}
