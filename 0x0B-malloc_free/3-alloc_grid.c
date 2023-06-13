b932b222dee319f9a04ef9c95536fab8b0509d9a
TheEikosystem <gloryedwinkpo@gmail.com>
2023-06-13 16:50:14 -0700

    malloc free

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,8 +10,7 @@ int **alloc_grid(int width, int height)
 {
 	int **mee;
 	int x, y;
-
-	if (width <= 0 || height <= 0)
+		if (width <= 0 || height <= 0)
 		return (NULL);
 	mee = malloc(sizeof(int *) * height);
 	if (mee == NULL)
@@ -19,19 +18,18 @@ int **alloc_grid(int width, int height)
 	for (x = 0; x < height; x++)
 	{
 		mee[x] = malloc(sizeof(int) * width);
-	if (mee[x] == NULL)
+		if (mee[x] == NULL)
 		{
-	for (; x >= 0; x--)
-		free(mee[x]);
-		free(mee);
+		for (; x >= 0; x--)
+			free(mee[x]);
+			free(mee);
 			return (NULL);
 		}
 	}
 		for (x = 0; x < height; x++)
 	{
 		for (y = 0; y < width; y++)
-			mee[x][y] = 0;
+		mee[x][y] = 0;
 	}
 	return (mee);
 }
-
